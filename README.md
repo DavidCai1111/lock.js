1d7a1d323335a7174dd4d875e5e25ec06d809a5e
DavidCai <davidcai1993@yahoo.com>
2016-08-15 22:12:40 +0800

    use PROJECT macro

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # lock.js
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
--- src/unlock.cc
+++ src/unlock.cc
@@ -1,13 +1,19 @@
 #include <node.h>
+#include <string>
+#include <iostream>
+#include "project.h"
+
+using namespace std;
 
 int main (int argc, char** argv) {
   const int scriptArgc = 5;
+
   char* scriptArgv[] = {
-    "node",
-    "-e",
-    SCRIPT,
-    SRC,
-    PUBLIC_KEY
+    const_cast<char*>("node"),
+    const_cast<char*>("-e"),
+    const_cast<char*>(SCRIPT),
+    const_cast<char*>(PROJECT),
+    const_cast<char*>(PUBLIC_KEY)
   };
 
   return node::Start(scriptArgc, scriptArgv);
