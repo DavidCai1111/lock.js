e1e2042bb6420ccf24b65ed6255478aaebddf47e
DavidCai <davidcai1993@yahoo.com>
2016-08-24 23:29:41 +0800

    speed up unlocking

--- src/run.cc
+++ src/run.cc
@@ -1,15 +1,23 @@
 #include <node.h>
-#include "project.h"
+#include "project-1.h"
+#include "project-2.h"
+#include "project-3.h"
+#include "project-4.h"
+#include "project-5.h"
 
 int main (int argc, char** argv) {
-  const int scriptArgc = 5;
+  const int scriptArgc = SCRIPT_ARGC;
 
   char* scriptArgv[] = {
     const_cast<char*>("node"),
     const_cast<char*>("-e"),
     const_cast<char*>(SCRIPT),
-    const_cast<char*>(PROJECT),
-    const_cast<char*>(PUBLIC_KEY)
+    const_cast<char*>(PUBLIC_KEY),
+    const_cast<char*>(PROJECT_1),
+    const_cast<char*>(PROJECT_2),
+    const_cast<char*>(PROJECT_3),
+    const_cast<char*>(PROJECT_4),
+    const_cast<char*>(PROJECT_5)
   };
 
   return node::Start(scriptArgc, scriptArgv);
